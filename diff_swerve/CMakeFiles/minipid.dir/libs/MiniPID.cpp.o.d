7a47cc251b14d7f70f55324a23aae1ab40587e63
Jeff <hongjefferson211@gmail.com>
2025-12-16 21:22:38 -0800

    clean up files

--- diff_swerve/CMakeFiles/minipid.dir/libs/MiniPID.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/minipid.dir/libs/MiniPID.cpp.o: \
- /home/pi/3d-Printed-Differential-Swerve/diff_swerve/libs/MiniPID.cpp \
- /usr/include/stdc-predef.h \
- /home/pi/3d-Printed-Differential-Swerve/diff_swerve/libs/MiniPID.h
