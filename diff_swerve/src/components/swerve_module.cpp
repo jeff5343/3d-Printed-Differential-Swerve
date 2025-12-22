b1de10c641406945dcf96f329addc5a0d825606b
Jeff <hongjefferson211@gmail.com>
2025-12-22 11:43:29 -0800

    clean up swerve_module files

--- diff_swerve/src/components/swerve_module.cpp
+++ diff_swerve/src/components/swerve_module.cpp
@@ -1,48 +1,60 @@
-
-#include "n20_motor.h"
 #include "swerve_module.h"
+#include "n20_motor.h"
 
 #include <cmath>
 
-
-SwerveModule::SwerveModule(SwerveModuleConstants moduleConstants) 
-	: moduleConstants(moduleConstants), 
-		rightMotor(moduleConstants.r_en, moduleConstants.r_in, moduleConstants.r_c1, moduleConstants.r_c2),
-		leftMotor(moduleConstants.l_en, moduleConstants.l_in, moduleConstants.l_c1, moduleConstants.l_c2),
-		rotationPid(moduleConstants.rotationPIDConstants.kP, 
-			moduleConstants.rotationPIDConstants.kI,
-			moduleConstants.rotationPIDConstants.kD,
-			moduleConstants.rotationPIDConstants.kF)
-{
-    PIDConstants motorPIDConstants = moduleConstants.motorPIDConstants;
-    rightMotor.setPIDF(motorPIDConstants.kP, motorPIDConstants.kI, motorPIDConstants.kD, motorPIDConstants.kF, motorPIDConstants.kKineticFriction, motorPIDConstants.kStaticFriction);
-    leftMotor.setPIDF(motorPIDConstants.kP, motorPIDConstants.kI, motorPIDConstants.kD, motorPIDConstants.kF, motorPIDConstants.kKineticFriction, motorPIDConstants.kStaticFriction);
-	this->rotationPid.setOutputLimits(-7, 7);
+SwerveModule::SwerveModule(SwerveModuleConstants moduleConstants)
+    : moduleConstants(moduleConstants),
+      rightMotor(moduleConstants.r_en, moduleConstants.r_in,
+                 moduleConstants.r_c1, moduleConstants.r_c2),
+      leftMotor(moduleConstants.l_en, moduleConstants.l_in,
+                moduleConstants.l_c1, moduleConstants.l_c2),
+      rotationPid(moduleConstants.rotationPIDConstants.kP,
+                  moduleConstants.rotationPIDConstants.kI,
+                  moduleConstants.rotationPIDConstants.kD,
+                  moduleConstants.rotationPIDConstants.kF) {
+  PIDConstants motorPIDConstants = moduleConstants.motorPIDConstants;
+  rightMotor.setPIDF(motorPIDConstants.kP, motorPIDConstants.kI,
+                     motorPIDConstants.kD, motorPIDConstants.kF,
+                     motorPIDConstants.kKineticFriction,
+                     motorPIDConstants.kStaticFriction);
+  leftMotor.setPIDF(motorPIDConstants.kP, motorPIDConstants.kI,
+                    motorPIDConstants.kD, motorPIDConstants.kF,
+                    motorPIDConstants.kKineticFriction,
+                    motorPIDConstants.kStaticFriction);
+  this->rotationPid.setOutputLimits(-7, 7);
 }
 
 float SwerveModule::getRotation() {
-	float rightMotorModuleRotations = this->rightMotor.getRotations() * SwerveModule::MODULE_ROTATION_PER_MOTOR_ROTATION;
-	float leftMotorModuleRotations = this->leftMotor.getRotations() * SwerveModule::MODULE_ROTATION_PER_MOTOR_ROTATION;
-	return rightMotorModuleRotations + leftMotorModuleRotations;
+  float rightMotorModuleRotations =
+      this->rightMotor.getRotations() *
+      SwerveModule::MODULE_ROTATION_PER_MOTOR_ROTATION;
+  float leftMotorModuleRotations =
+      this->leftMotor.getRotations() *
+      SwerveModule::MODULE_ROTATION_PER_MOTOR_ROTATION;
+  return rightMotorModuleRotations + leftMotorModuleRotations;
 }
 
 // TODO: going to fastest angle reverses driving direction as well
 void SwerveModule::setTargetRotation(float target) {
-    float moduleRotation = this->getRotation();
-    float closestTarget = this->calculateClosestMatchingRotation(target);
-	double velOutput = rotationPid.getOutput(moduleRotation, closestTarget);
+  float moduleRotation = this->getRotation();
+  float closestTarget = this->calculateClosestMatchingRotation(target);
+  double velOutput = rotationPid.getOutput(moduleRotation, closestTarget);
 
-	float rotError = abs(moduleRotation - closestTarget);
+  float rotError = abs(moduleRotation - closestTarget);
 
-	// help start moving motors
-	if (rotError > 0.01 && (rightMotor.getRps() < 0.01 || leftMotor.getRps() < 0.01)) {
-		velOutput += copysign(1.0, velOutput) * this->moduleConstants.rotationPIDConstants.kStaticFriction;
-	} else {
-		velOutput += copysign(1.0, velOutput) * this->moduleConstants.rotationPIDConstants.kKineticFriction;
-	}
+  // help start moving motors
+  if (rotError > 0.01 &&
+      (rightMotor.getRps() < 0.01 || leftMotor.getRps() < 0.01)) {
+    velOutput += copysign(1.0, velOutput) *
+                 this->moduleConstants.rotationPIDConstants.kStaticFriction;
+  } else {
+    velOutput += copysign(1.0, velOutput) *
+                 this->moduleConstants.rotationPIDConstants.kKineticFriction;
+  }
 
-    rightMotor.setTargetVelocity(-velOutput);
-    leftMotor.setTargetVelocity(-velOutput);
+  rightMotor.setTargetVelocity(-velOutput);
+  leftMotor.setTargetVelocity(-velOutput);
 }
 
 void SwerveModule::setTargetVelocity(float target) {
@@ -56,23 +68,24 @@ void SwerveModule::setPercentOut(float percentOut) {
 }
 
 float SwerveModule::calculateClosestMatchingRotation(float target) {
-    float moduleRotation = this->getRotation();
-    float whole;
-	float relativeRotation = std::modf(moduleRotation, &whole);
-	float diff =  target - relativeRotation;
-	// force difference to be between [-0.5, 0.5]
-	if (diff > 0.5) {
-		diff -= 1;
-	} else if (diff < -0.5) {
-		diff += 1;
-	}
-	float finalTarget = moduleRotation + diff;
-	return finalTarget;
+  float moduleRotation = this->getRotation();
+	// split rotation into whole number and decimal
+  float whole;
+  float relativeRotation = std::modf(moduleRotation, &whole);
+  float diff = target - relativeRotation;
+  // force difference to be between [-0.5, 0.5]
+  if (diff > 0.5) {
+    diff -= 1;
+  } else if (diff < -0.5) {
+    diff += 1;
+  }
+  float finalTarget = moduleRotation + diff;
+  return finalTarget;
 }
 
 void SwerveModule::resetPosition() {
-  this->rightMotor.setRotations(0);		
-  this->leftMotor.setRotations(0);		
+  this->rightMotor.setRotations(0);
+  this->leftMotor.setRotations(0);
 }
 
 float SwerveModule::getErrorToTargetRotation(float target) {
@@ -80,6 +93,6 @@ float SwerveModule::getErrorToTargetRotation(float target) {
 }
 
 void SwerveModule::stop() {
-	this->rightMotor.setPercentOut(0);
-	this->leftMotor.setPercentOut(0);
+  this->rightMotor.setPercentOut(0);
+  this->leftMotor.setPercentOut(0);
 }
--- diff_swerve/src/components/swerve_module.h
+++ diff_swerve/src/components/swerve_module.h
@@ -6,44 +6,45 @@
 #include "n20_motor.h"
 #include "util.h"
 
-class SwerveModuleConstants {
-  public:
-    int r_en, r_in, r_c1, r_c2;
-    int l_en, l_in, l_c1, l_c2;
-    PIDConstants motorPIDConstants;
-    PIDConstants rotationPIDConstants;
-    SwerveModuleConstants(
-        int r_en, int r_in, int r_c1, int r_c2,
-        int l_en, int l_in, int l_c1, int l_c2,
-        PIDConstants motorPIDConstants, PIDConstants rotationPIDConstants)
-        : r_en(r_en), r_in(r_in), r_c1(r_c1), r_c2(r_c2),
-          l_en(l_en), l_in(l_in), l_c1(l_c1), l_c2(l_c2),
-          motorPIDConstants(motorPIDConstants), rotationPIDConstants(rotationPIDConstants) {};
-    SwerveModuleConstants()
-        : r_en(0), r_in(0), r_c1(0), r_c2(0),
-          l_en(0), l_in(0), l_c1(0), l_c2(0),
-          motorPIDConstants(), rotationPIDConstants() {};
+class SwerveModule {
+private:
+  static constexpr float MODULE_ROTATION_PER_MOTOR_ROTATION =
+      (17.0 / 60.0) / 2.0;
+
+  SwerveModuleConstants moduleConstants;
+  MiniPID rotationPid;
+
+  float calculateClosestMatchingRotation(float target);
+
+public:
+  N20Motor rightMotor, leftMotor;
+
+  SwerveModule(SwerveModuleConstants moduleConstants);
+  float getRotation();
+  void setTargetRotation(float target);
+  void setTargetVelocity(float target);
+  void setPercentOut(float percentOut);
+  void resetPosition();
+  float getErrorToTargetRotation(float target);
+  void stop();
 };
 
-class SwerveModule {
-  private:
-    static constexpr float MODULE_ROTATION_PER_MOTOR_ROTATION = (17.0 / 60.0) / 2.0;
-
-    SwerveModuleConstants moduleConstants;
-    MiniPID rotationPid;
-
-    float calculateClosestMatchingRotation(float target);
-  public:
-    N20Motor rightMotor, leftMotor;
-
-    SwerveModule(SwerveModuleConstants moduleConstants);
-    float getRotation();
-    void setTargetRotation(float target);
-    void setTargetVelocity(float target);
-    void setPercentOut(float percentOut);
-    void resetPosition();
-    float getErrorToTargetRotation(float target);
-    void stop();
+class SwerveModuleConstants {
+public:
+  int r_en, r_in, r_c1, r_c2;
+  int l_en, l_in, l_c1, l_c2;
+  PIDConstants motorPIDConstants;
+  PIDConstants rotationPIDConstants;
+  SwerveModuleConstants(int r_en, int r_in, int r_c1, int r_c2, int l_en,
+                        int l_in, int l_c1, int l_c2,
+                        PIDConstants motorPIDConstants,
+                        PIDConstants rotationPIDConstants)
+      : r_en(r_en), r_in(r_in), r_c1(r_c1), r_c2(r_c2), l_en(l_en), l_in(l_in),
+        l_c1(l_c1), l_c2(l_c2), motorPIDConstants(motorPIDConstants),
+        rotationPIDConstants(rotationPIDConstants) {};
+  SwerveModuleConstants()
+      : r_en(0), r_in(0), r_c1(0), r_c2(0), l_en(0), l_in(0), l_c1(0), l_c2(0),
+        motorPIDConstants(), rotationPIDConstants() {};
 };
 
 #endif
