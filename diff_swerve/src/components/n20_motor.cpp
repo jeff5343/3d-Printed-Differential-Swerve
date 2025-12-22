735372ab46d1ce33c599480cbc56d9e8bc429ad0
Jeff <hongjefferson211@gmail.com>
2025-12-22 11:32:37 -0800

    clean up n20_motor files

--- diff_swerve/src/components/n20_motor.cpp
+++ diff_swerve/src/components/n20_motor.cpp
@@ -1,115 +1,118 @@
+#include <algorithm>
 #include <iostream>
 #include <math.h>
 #include <pigpio.h>
 #include <thread>
-#include <algorithm>
 
 #include "MiniPID.h"
-#include "n20_motor.h"
 #include "logger.h"
+#include "n20_motor.h"
 #include "util.h"
 
+N20Motor::N20Motor(int en, int in, int c1, int c2)
+    : en(en), in(in), c1(c1), c2(c2), ticks(0), rotations(0),
+      lastVelUpdateRotations(0), pid(MiniPID(0, 0, 0)) {
+  lastVelUpdateMicros = Util::micros();
 
-static void updateRotationCallback(int gpio, int level, uint32_t _tick, void *userdata) {
-	N20Motor *motor = (N20Motor*) userdata;
+  gpioSetMode(en, PI_OUTPUT);
+  gpioSetMode(in, PI_OUTPUT);
+  gpioSetMode(c1, PI_INPUT);
+  gpioSetMode(c2, PI_INPUT);
 
-	if (gpio == motor->c1) {
-		motor->levC1 = level;
-	} else {
-		motor->levC2 = level;
-	}
+  gpioSetAlertFuncEx(c1, updateRotationCallback, this);
+  gpioSetAlertFuncEx(c2, updateRotationCallback, this);
 
-	// debounce
-	if (gpio == motor->lastLevGPIO) {
-		return;
-	}
-	motor->lastLevGPIO = gpio;
+  pid.setOutputLimits(-1, 1);
 
-	if (gpio == motor->c1) {
-		if (level == 1) {
-			if (motor->levC2 == 1) motor->ticks++;
-			else motor->ticks--;
-		} else {
-			if (motor->levC2 == 0) motor->ticks++;
-			else motor->ticks--;
-		}
-	} else if (gpio == motor->c2) {
-		if (level == 1) {
-			if (motor->levC1 == 1) motor->ticks--;
-			else motor->ticks++;
-		} else {
-			if (motor->levC1 == 0) motor->ticks--;
-			else motor->ticks++;
-		}
-
-	}
-	motor->rotations = motor->ticks * N20Motor::ROTATIONS_TO_TICKS;
+  running = true;
+  updateVelocityThread = std::thread(&N20Motor::updateVelocityThreadCall, this);
 }
 
-void N20Motor::updateVelocityThread() {
-	while (true) {
-		double currMicros = Util::micros();
-		double currRotations = getRotations();
-		double diffRotation = currRotations - this->lastVelUpdateRotations;
-		double diffSec = (currMicros - this->lastVelUpdateMicros) / 1000000.0;
-
-		this->rps = diffRotation / diffSec;
-
-		this->lastVelUpdateMicros = currMicros;
-		this ->lastVelUpdateRotations = currRotations;
-
-		gpioDelay(20000);
-	}
-}
-
-N20Motor::N20Motor(int en, int in, int c1, int c2) : pid(MiniPID(0, 0, 0)) {
-	this->en = en;
-	this->in = in;
-	this->c1 = c1;
-	this->c2 = c2;
-	this->ticks = 0;
-	this->rotations = 0;
-
-	lastVelUpdateMicros = Util::micros();
-	lastVelUpdateRotations = getRotations();
-
-	gpioSetMode(en, PI_OUTPUT);
-	gpioSetMode(in, PI_OUTPUT);
-	gpioSetMode(c1, PI_INPUT);
-	gpioSetMode(c2, PI_INPUT);
-
-	gpioSetAlertFuncEx(c1, updateRotationCallback, this);
-	gpioSetAlertFuncEx(c2, updateRotationCallback, this);
-
-	pid.setOutputLimits(-1, 1);
-
-	std::thread updateVelocityThread(&N20Motor::updateVelocityThread, this);
-	updateVelocityThread.detach();
-}
-
-void N20Motor::setPIDF(float kP, float kI, float kD, float kF, float kKineticFriction, float kStaticFriction) {
-	pid.setP(kP);
-	pid.setI(kI);
-	pid.setD(kD);
-	pid.setF(kF);
-	this->kKineticFriction = kKineticFriction;
-	this->kStaticFriction = kStaticFriction;
+void N20Motor::setPIDF(float kP, float kI, float kD, float kF,
+                       float kKineticFriction, float kStaticFriction) {
+  pid.setP(kP);
+  pid.setI(kI);
+  pid.setD(kD);
+  pid.setF(kF);
+  this->kKineticFriction = kKineticFriction;
+  this->kStaticFriction = kStaticFriction;
 }
 
 void N20Motor::setPercentOut(float percentOut) {
-	double clamped = std::clamp(percentOut, -1.0f, 1.0f);
-	gpioWrite(in, clamped >= 0);
-	gpioPWM(en, (int) Util::mapIntoRange(fabs(clamped), 0, 1, 0, 255)); 
-	Logger::logger() << clamped << std::endl;
+  double clamped = std::clamp(percentOut, -1.0f, 1.0f);
+  gpioWrite(in, clamped >= 0);
+  gpioPWM(en, (int)Util::mapIntoRange(fabs(clamped), 0, 1, 0, 255));
+  Logger::logger() << clamped << std::endl;
 }
 
 void N20Motor::setTargetVelocity(float targetRps) {
-	double output = pid.getOutput(rps, targetRps);
-	double sign = copysign(1.0, output); 
-	if (abs(this->getRps()) < 0.00001) {
-		output += sign * this->kStaticFriction;
-	} else {
-		output += sign * this->kKineticFriction;
-	}
-	setPercentOut(output);
+  double output = pid.getOutput(rps, targetRps);
+  double sign = copysign(1.0, output);
+  if (abs(this->getRps()) < 0.00001) {
+    output += sign * this->kStaticFriction;
+  } else {
+    output += sign * this->kKineticFriction;
+  }
+  setPercentOut(output);
+}
+
+void N20Motor::updateVelocityThreadCall() {
+  while (running) {
+    double currMicros = Util::micros();
+    double currRotations = getRotations();
+    double diffRotation = currRotations - this->lastVelUpdateRotations;
+    double diffSec = (currMicros - this->lastVelUpdateMicros) / 1000000.0;
+
+    this->rps = diffRotation / diffSec;
+
+    this->lastVelUpdateMicros = currMicros;
+    this->lastVelUpdateRotations = currRotations;
+
+    // 20 milliseconds
+    gpioDelay(20000);
+  }
+}
+
+static void updateRotationCallback(int gpio, int level, uint32_t _tick,
+                                   void *userdata) {
+  N20Motor *motor = (N20Motor *)userdata;
+  if (gpio == motor->c1) {
+    motor->levC1 = level;
+  } else {
+    motor->levC2 = level;
+  }
+
+  // debounce
+  if (gpio == motor->lastLevGPIO) {
+    return;
+  }
+  motor->lastLevGPIO = gpio;
+
+  // quadrature encoder
+  if (gpio == motor->c1) {
+    if (level == 1) {
+      if (motor->levC2 == 1)
+        motor->ticks++;
+      else
+        motor->ticks--;
+    } else {
+      if (motor->levC2 == 0)
+        motor->ticks++;
+      else
+        motor->ticks--;
+    }
+  } else if (gpio == motor->c2) {
+    if (level == 1) {
+      if (motor->levC1 == 1)
+        motor->ticks--;
+      else
+        motor->ticks++;
+    } else {
+      if (motor->levC1 == 0)
+        motor->ticks--;
+      else
+        motor->ticks++;
+    }
+  }
+  motor->rotations = motor->ticks * N20Motor::ROTATIONS_TO_TICKS;
 }
--- diff_swerve/src/components/n20_motor.h
+++ diff_swerve/src/components/n20_motor.h
@@ -4,39 +4,43 @@
 #include "MiniPID.h"
 
 #include "logger.h"
+#include <atomic>
 #include <iostream>
 
 class N20Motor {
-	public:
-		// constants
-		static constexpr float ROTATIONS_TO_TICKS = (1.0 / 200.0) * (1.0 / 4.0);
-		// GPIOs
-		int en;
-		int in, c1, c2;
-		// data
-		volatile float kKineticFriction, kStaticFriction;
-		volatile int ticks;
-		volatile int levC1, levC2;
-		volatile int lastLevGPIO;
-		volatile float rotations, rps;
-		volatile float lastVelUpdateRotations, lastVelUpdateMicros;
-		// PID
-		MiniPID	pid;
+public:
+  N20Motor(int en, int in, int c1, int c2);
+  N20Motor() : en(0), in(0), c1(0), c2(0) {};
+  void setPIDF(float kP, float kI, float kD, float kF, float kKineticFriction,
+               float kStaticFriction);
+  void setPercentOut(float percentOut);
+  void setTargetVelocity(float targetRps);
+  float getRotations() { return rotations; }
+  void setRotations(float rotations) {
+    this->ticks = rotations * (1.0 / ROTATIONS_TO_TICKS);
+    this->rotations = rotations;
+  }
+  float getRps() { return rps; }
+  int getTicks() { return ticks; }
 
-		// functions
-		N20Motor(int en, int in, int c1, int c2);
-		N20Motor() : en(0), in(0), c1(0), c2(0) {};
-		void setPIDF(float kP, float kI, float kD, float kF, float kKineticFriction, float kStaticFriction);
-		void setPercentOut(float percentOut);
-		void setTargetVelocity(float targetRps);
-		void updateVelocityThread();
-		float getRotations() { return rotations; }
-		void setRotations(float rotations) {
-			this->ticks = rotations * (1.0 / ROTATIONS_TO_TICKS);
-			this->rotations = rotations;
-		}
-		float getRps() { return rps; }
-		int getTicks() { return ticks; }
+  // constants
+  static constexpr float ROTATIONS_TO_TICKS = (1.0 / 200.0) * (1.0 / 4.0);
+  // GPIOs
+  int en;
+  int in, c1, c2;
+  // data
+  float kKineticFriction, kStaticFriction;
+  std::atomic<int> ticks;
+  std::atomic<int> levC1, levC2;
+  std::atomic<int> lastLevGPIO;
+  std::atomic<float> rotations, rps;
+  std::atomic<float> lastVelUpdateRotations, lastVelUpdateMicros;
+  // PID
+  MiniPID pid;
+  // thread
+  std::atomic<bool> running;
+  std::thread updateVelocityThread;
+  void updateVelocityThreadCall();
 };
 
 #endif
